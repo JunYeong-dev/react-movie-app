c3acb3757c391b39b09e306a3df62c9c93f82c6a
Jun-cloud <kjy8827@gmail.com>
2020-09-25 21:19:35 +0900

    Planning movie app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Hello</h1>
-  );
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    moview: []
+  }
+  componentDidMount() {
+    // setTimeout() : JavaScript함수
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  render() {
+    // ES6방식 : 원래라면 this.state.isLoading 으로 state에 접근해야 하지만 아래와 같은 ES6방식으로도 접근할 수 있음
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
