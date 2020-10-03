72638d3858758ff24b3c179092cc146680d0e5e2
Jun-cloud <kjy8827@gmail.com>
2020-10-03 10:11:07 +0900

    Updates

---
+++ precache-manifest.6a437de1462fa5bb30860ed952f2c4dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12dce38d29f9730e6a57056ef2037997",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "3d10f0bbaebfd9d0ca8a",
+    "url": "/react-movie-app/static/css/main.1254f802.chunk.css"
+  },
+  {
+    "revision": "15d182d7daeb750b610b",
+    "url": "/react-movie-app/static/js/2.830f6f74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-app/static/js/2.830f6f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d10f0bbaebfd9d0ca8a",
+    "url": "/react-movie-app/static/js/main.ee6eed43.chunk.js"
+  },
+  {
+    "revision": "651af530d224a688009d",
+    "url": "/react-movie-app/static/js/runtime-main.c0625075.js"
+  }
+]);
