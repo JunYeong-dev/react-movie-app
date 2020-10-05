3561a13ddc95ad87e54f0b5fbc8eb897543e0a65
Jun-cloud <kjy8827@gmail.com>
2020-10-05 20:46:35 +0900

    React router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
 
 function App(){
-  return <div>React</div>
+  return (
+    <HashRouter>
+      {/* 꼭 path와 component의 이름이 같을 필요는 없음 */}
+      <Route path="/about" component={ About } />
+    </HashRouter>
+  );
 }
 
 export default App;
