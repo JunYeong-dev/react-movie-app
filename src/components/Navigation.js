9d103f5c1ef7fdcec07a88347c7688d49c06ea31
Jun-cloud <kjy8827@gmail.com>
2020-10-06 21:08:19 +0900

    React Link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navigation(){
     return (
         <div>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
+            {/* html의 태그의 경우 새로고침을 하게 되어 React도 다시 시작하게 만들기 때문에 React의 Link를 사용 */}
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
         </div>
     );
 }
