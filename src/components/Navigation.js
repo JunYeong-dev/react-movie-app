3f804836a6c00436cb0f9b077dc1135a1f871c92
Jun-cloud <kjy8827@gmail.com>
2020-10-09 17:02:13 +0900

    Sharing props

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,12 @@ function Navigation(){
         <div className="nav">
             {/* html의 태그의 경우 새로고침을 하게 되어 React도 다시 시작하게 만들기 때문에 React의 Link를 사용 */}
             <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+            <Link to={{
+                pathname: "/about",
+                state: {
+                    fromNavigation: true
+                }
+            }}>About</Link>
         </div>
     );
 }
