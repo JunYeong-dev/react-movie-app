f9edc7359d4df1b50530b50edd0ce0497749dd12
Jun-cloud <kjy8827@gmail.com>
2020-10-14 21:09:03 +0900

    fix css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,17 +5,17 @@ import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }){
     return (
-    <Link to={{
-        pathname: `/movie/${ id }`,
-        state: {
-            year,
-            title,
-            summary,
-            poster,
-            genres
-        }
-    }}>
         <div className="movie">
+            <Link to={{
+                pathname: `/movie/${ id }`,
+                state: {
+                    year,
+                    title,
+                    summary,
+                    poster,
+                    genres
+                }
+            }}>
             <img src={ poster } alt={ title } title={ title }></img>
             <div className="movie_data">
                 <h3 className="movie_title">{ title }</h3>
@@ -28,8 +28,8 @@ function Movie({ id, year, title, summary, poster, genres }){
                 </ul>
                 <p className="movie_summary">{ summary.slice(0, 140) }...</p>
             </div>
-        </div>
-    </Link>
+        </Link>
+    </div>
     );
 }
 
